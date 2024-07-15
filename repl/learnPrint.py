f7dfe2a4fe823ba3edc45b1dbd68337260a23bf9
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-07-15 15:51:20 -0400

    Create learnPrint.py

---
+++ repl/learnPrint.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " " + "Angela")
+input("What is your name? ")
+print("Hello " + input("What is your name? "))
