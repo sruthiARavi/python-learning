1327d0864fea54bd3e87797412f48088147e1d47
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-07-22 22:42:49 -0400

    Update add-even-nos.py

--- auditorium/day-5/add-even-nos.py
+++ auditorium/day-5/add-even-nos.py
@@ -3,7 +3,8 @@ target = int(input()) # Enter a number between 0 and 1000
 
 # Write your code here 👇
 sum = 0
-for number in range(1,(target+1)):
-  if(number%2 == 0):
+#for number in range(1,(target+1)):
+#  if(number%2 == 0):
+for number in range(2,(target+1), 2):
     sum += number
 print(sum)
