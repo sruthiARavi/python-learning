8ea681d4d757a723dc07f6ade9acf1dfaed1b13f
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-07-22 21:41:00 -0400

    Create heads-tails.py

---
+++ auditorium/day-4/heads-tails.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+choice = random.randint(0,1)
+
+coin = ["Heads", "Tails"]
+
+print(f"{coin[choice]}")
