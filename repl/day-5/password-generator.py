a0c431d53b91535bab7d46c2bea47c71beb08cfd
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-07-22 23:21:09 -0400

    Update password-generator.py

--- repl/day-5/password-generator.py
+++ repl/day-5/password-generator.py
@@ -14,39 +14,34 @@ pwd_symbols = []
 pwd_numbers = []
 
 for i in range(0, nr_letters):
-  pwd_letters.append(letters[random.randint(0, len(letters)-1)])
+  #pwd_letters.append(letters[random.randint(0, len(letters)-1)])
+  #Alternatively, you can concatenate to the final password string directly 
+  pwd_letters.append(random.choice(letters))
 
 for i in range(0, nr_symbols):
-  pwd_symbols.append(symbols[random.randint(0, len(symbols)-1)])
+  #pwd_symbols.append(symbols[random.randint(0, len(symbols)-1)])
+  pwd_symbols.append(random.choice(symbols))
 
 for i in range(0, nr_numbers):
-  pwd_numbers.append(numbers[random.randint(0, len(numbers)-1)])
+  #pwd_numbers.append(numbers[random.randint(0, len(numbers)-1)])
+  pwd_numbers.append(random.choice(numbers))
   
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 pwd_easy = pwd_letters + pwd_symbols + pwd_numbers
-pwd_easy_str = ""
-for i in range(0, len(pwd_easy)):
-  pwd_easy_str += pwd_easy[i]
+pwd_easy_str = ''.join(pwd_easy)
 print(f"Your password is (easy): {pwd_easy_str}")
+
+#or 
+#for i in range(0, len(pwd_easy)):
+  #pwd_easy_str += pwd_easy[i]
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-#Trying shuffle 
+#Using shuffle 
 #https://stackoverflow.com/questions/2668312/shuffle-string-in-python
-easy_pwd_chars = list(pwd_easy_str)
-random.shuffle(easy_pwd_chars)
-pwd_hard_str = ''.join(easy_pwd_chars)
+#easy_pwd_chars = list(pwd_easy_str)
+random.shuffle(pwd_easy)
+pwd_hard_str = ''.join(pwd_easy)
 print(f"Your password is (hard): {pwd_hard_str}")  
-
-#This is not the right implementation as there is no way to ensure that the requirements are respected. 
-#pwd_hard = []
-#for i in range(0, len(pwd_easy)):
-  #pwd_hard.append(pwd_easy[random.randint(0, len(pwd_easy)-1)])
-
-#pwd_hard_str = ""
-#for i in range(0, len(pwd_hard)):
-  #pwd_hard_str += pwd_hard[i]
-
-#print(f"Your password is (hard): {pwd_hard_str}")
