5d6e353b52a90be2796db0223985490c6570add8
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-07-22 22:52:50 -0400

    Update fizzbuzz.py

--- auditorium/day-5/fizzbuzz.py
+++ auditorium/day-5/fizzbuzz.py
@@ -1,11 +1,23 @@
 # Write your code here 👇
-print("1")
-for number in range(2, 101):
-  output=""; 
-  if(number%3 == 0):
-    output += "Fizz"
-  if(number%5 == 0):
-    output += "Buzz"
-  if(output == ""):
-    output = str(number)
-  print(output)
+for number in range(1, 101):
+  if(number%3 == 0 and number%5 ==0):
+    print("FizzBuzz")
+  elif(number%3 == 0):
+    print("Fizz")
+  elif(number%5 == 0):
+    print("Buzz")
+  else:
+    print(number)
+
+#or 
+
+#print("1")
+#for number in range(2, 101):
+  #output=""; 
+  #if(number%3 == 0):
+    #output += "Fizz"
+  #if(number%5 == 0):
+    #output += "Buzz"
+  #if(output == ""):
+    #output = str(number)
+  #print(output)
