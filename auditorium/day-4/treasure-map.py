92c6fcdac06d341c76b72eb4cb9303c082c7f4c7
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-07-22 21:30:35 -0400

    Create treasure-map.py

---
+++ auditorium/day-4/treasure-map.py
@@ -0,0 +1,19 @@
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+pos1 = int(position[1])-1;
+
+abc = ["A", "B", "C"]
+pos2 = position[0].upper()
+pos2_int = abc.index(pos2) 
+
+map[pos1][pos2_int] = "X"
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
