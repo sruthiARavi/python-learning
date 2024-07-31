b33416bad1b9a915835b5b3747682cc540184b9a
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-07-31 17:17:17 -0400

    Create draw-a-square.py

---
+++ reeborg/day-6/draw-a-square.py
@@ -0,0 +1,20 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_around() #Or you could just do turn_left() call thrice 
+    turn_left()
+    
+
+#draw a square 
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+    
