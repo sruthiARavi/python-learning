480218d163aeb6f3b04ad2670f665c8eacf7ad86
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-01 18:09:25 -0400

    Create maze-challenge.py

---
+++ reeborg/day-6/maze-challenge.py
@@ -0,0 +1,18 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_around()
+    turn_left()
+    
+#maze challenge 
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+while not at_goal(): 
+    if right_is_clear(): 
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()    
+    else:                    
+        turn_left()
