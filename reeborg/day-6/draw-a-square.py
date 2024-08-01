19fc8e51362e8514bdcb576d05948e33e5a91a6a
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-01 09:46:29 -0400

    Update draw-a-square.py

--- reeborg/day-6/draw-a-square.py
+++ reeborg/day-6/draw-a-square.py
@@ -8,6 +8,7 @@ def turn_right():
     
 
 #draw a square 
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
 turn_left()
 move()
 turn_right()
