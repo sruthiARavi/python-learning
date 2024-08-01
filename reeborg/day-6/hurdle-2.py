6b0cb05d0ee33237873d3bad1fc989b029e6969f
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-01 09:49:57 -0400

    Update hurdle-2.py

--- reeborg/day-6/hurdle-2.py
+++ reeborg/day-6/hurdle-2.py
@@ -21,7 +21,8 @@ def jump():
 
 #hurdle-2 challenge 
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
-while at_goal() is not True:
+#while at_goal() is not True:
+while not at_goal():
     jump()
 
     
