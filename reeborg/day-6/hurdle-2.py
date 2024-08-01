b86305104d782fa0d12556960c827bc27015eaf6
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-01 09:45:03 -0400

    Create hurdle-2.py

---
+++ reeborg/day-6/hurdle-2.py
@@ -0,0 +1,26 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_around()
+    turn_left()
+    
+def jump():
+    #first and last line of the function 
+    #only for the purposes of this specific 
+    #requirement 
+    move()  
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+#hurdle-2 challenge 
+while at_goal() is not True:
+    jump()
+
+    
