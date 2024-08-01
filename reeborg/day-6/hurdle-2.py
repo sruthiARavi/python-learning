9c421998452c15e13611233c378f90ec5d195239
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-01 09:45:38 -0400

    Update hurdle-2.py

--- reeborg/day-6/hurdle-2.py
+++ reeborg/day-6/hurdle-2.py
@@ -20,6 +20,7 @@ def jump():
     turn_left()
 
 #hurdle-2 challenge 
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
 while at_goal() is not True:
     jump()
 
