976516faf855346f58338f34f8ce1e904ad604d7
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-01 22:32:44 -0400

    Update maze-challenge.py

--- reeborg/day-6/maze-challenge.py
+++ reeborg/day-6/maze-challenge.py
@@ -8,6 +8,18 @@ def turn_right():
     
 #maze challenge 
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+#edge-case : 
+#No hurdles on any side for single step moves i.e. stuck in a loop (keep going in a square) 
+while front_is_clear():
+    move()
+    
+turn_left()
+
+#TODO : This still doesn't handle subseqent loops
+#Only backtracking will work 
+
+#solution 
 while not at_goal(): 
     if right_is_clear(): 
         turn_right()
