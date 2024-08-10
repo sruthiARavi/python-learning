2d822f5bb82dc7c4d3a5b53e1adca85236809fdb
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-09 20:10:34 -0400

    Update learn-dictionary.py

--- repl/day-9/learn-dictionary.py
+++ repl/day-9/learn-dictionary.py
@@ -13,7 +13,6 @@ print(empty_dictionary)
 #edit a dictionary
 my_dictionary["key1"] = "1a"
 
-print("******************")
 #loop through a dictionary
 #1. print keys
 for key in my_dictionary:
@@ -21,9 +20,42 @@ for key in my_dictionary:
 #2. print vals
 for key in my_dictionary:
     print(f"{key} --> {my_dictionary[key]}")
-print("******************")
 
 print(my_dictionary)
 #wipe out a dictionary
 my_dictionary = {}
 print(my_dictionary)
+
+#####################################
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+#nested dictionary
+travel_log = {
+    "France" : ["Paris", "Lille", "Dijon"],
+    "Germany" : ["Berlin", "Stuttgart"],
+}
+
+for country in travel_log:
+    if country == "France":
+        city = travel_log[country][1]
+        print(city)
+
+nested_list = ["A", "B", ["C", "D"]]
+print(nested_list[2][1])
+
+travel_log["nest_list"] = nested_list
+print(travel_log["nest_list"][2][1])
+
+travel_log["France"] = {
+    "num_times_visited": 8,
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+}
+travel_log["Germany"] = {
+    "num_times_visited": 5,
+    "cities_visited": ["Berlin", "Stuttgart"],
+}
+print(travel_log["Germany"]["cities_visited"][1])
